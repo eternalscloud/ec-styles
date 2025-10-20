953ff73d34f8fa41f438769c65b31525bd071929
Chandra Sekhar <gchandrasekhar72@gmail.com>
2025-10-20 15:46:03 +0530

    Initial commit

---
+++ src/components/Badge/Badge.tsx
@@ -0,0 +1,119 @@
+import { classNames } from 'primereact/utils';
+import './Badge.scss';
+
+type TSeverity = 'pending' | 'confirmed' | 'denied' | 'hold' | 'warning' | 'primary';
+
+interface IBadgeIcon {
+  icon: string;
+  iconPosition?: 'start' | 'end';
+}
+
+interface IBadge extends Partial<IBadgeIcon> {
+  title: string;
+}
+interface IBadgeOptions {
+  severity: TSeverity;
+  badge: IBadge;
+  onClick?: () => void;
+}
+
+interface IBadgeSplitOptions extends Omit<IBadgeOptions, 'badge'> {
+  badge: {
+    label: string;
+    labelIcon?: IBadgeIcon;
+    value: string;
+    valueIcon?: IBadgeIcon;
+  };
+}
+
+function BadgeIcon(options: IBadgeIcon) {
+  return (
+    <div className={`ec-badge-icon-${options.iconPosition}`}>
+      <i className={options.icon}></i>
+    </div>
+  );
+}
+
+function BadgeSolid(options: IBadgeOptions) {
+  const iconPosition = options?.badge?.iconPosition || 'start';
+  const badgeID = `ec-badge-${options.severity}-${options.badge.title}`;
+
+  return (
+    <div className={`ec-badge-solid-${options.severity}`} id={badgeID} onClick={options?.onClick}>
+      {options?.badge?.icon && iconPosition === 'start' && <BadgeIcon icon={options.badge.icon} iconPosition={iconPosition} />}
+      {options?.badge?.title && <div className="ec-badge-title">{options.badge.title}</div>}
+      {options?.badge?.icon && iconPosition === 'end' && <BadgeIcon icon={options.badge.icon} iconPosition={iconPosition} />}
+    </div>
+  );
+}
+
+function BadgeLight(options: IBadgeOptions) {
+  const iconPosition = options?.badge?.iconPosition || 'start';
+  const badgeID = `ec-badge-${options.severity}-${options.badge.title}`;
+
+  return (
+    <div className={`ec-badge-light-${options.severity}`} id={badgeID} onClick={options?.onClick}>
+      {options?.badge?.icon && iconPosition === 'start' && <BadgeIcon icon={options.badge.icon} iconPosition={iconPosition} />}
+      {options?.badge?.title && <div className="ec-badge-title">{options.badge.title}</div>}
+      {options?.badge?.icon && iconPosition === 'end' && <BadgeIcon icon={options.badge.icon} iconPosition={iconPosition} />}
+    </div>
+  );
+}
+
+function BadgeOutline(options: IBadgeOptions) {
+  const iconPosition = options?.badge?.iconPosition || 'start';
+  const badgeID = `ec-badge-${options.severity}-${options.badge.title}`;
+
+  return (
+    <div className={`ec-badge-outline-${options.severity}`} id={badgeID} onClick={options?.onClick}>
+      {options?.badge?.icon && iconPosition === 'start' && <BadgeIcon icon={options.badge.icon} iconPosition={iconPosition} />}
+      {options?.badge?.title && <div className="ec-badge-title">{options.badge.title}</div>}
+      {options?.badge?.icon && iconPosition === 'end' && <BadgeIcon icon={options.badge.icon} iconPosition={iconPosition} />}
+    </div>
+  );
+}
+
+function BadgeLink(options: IBadgeOptions) {
+  const iconPosition = options?.badge?.iconPosition || 'start';
+  const badgeID = `ec-badge-${options.severity}-${options.badge.title}`;
+
+  return (
+    <div className={`ec-badge-link-${options.severity}`} id={badgeID} onClick={options?.onClick}>
+      {options?.badge?.icon && iconPosition === 'start' && <BadgeIcon icon={options.badge.icon} iconPosition={iconPosition} />}
+      {options?.badge?.title && <div className="ec-badge-title">{options.badge.title}</div>}
+      {options?.badge?.icon && iconPosition === 'end' && <BadgeIcon icon={options.badge.icon} iconPosition={iconPosition} />}
+    </div>
+  );
+}
+
+function BadgePreset(options: IBadgeOptions) {
+  const badgeID = `ec-badge-${options.severity}-${options.badge.title}`;
+  return (
+    <div className={`ec-badge-preset-${options.severity}`} id={badgeID} onClick={options?.onClick}>
+      {options?.badge?.title && <div className="ec-badge-title">{options.badge.title}</div>}
+    </div>
+  );
+}
+
+function BadgeSplit(options: IBadgeSplitOptions) {
+  const iconPositionLabel = options?.badge?.labelIcon?.iconPosition || 'start';
+  const iconPositionValue = options?.badge?.valueIcon?.iconPosition || 'start';
+  const badgeID = `ec-badge-${options.severity}-${options.badge.label}`;
+  const badgeClass = `ec-badge-split-${options.severity}`;
+  return (
+    <div className={badgeClass} id={badgeID} onClick={options?.onClick}>
+      <div className={classNames('ec-badge-split-label', `${badgeClass}-label`)}>
+        {options?.badge?.labelIcon?.icon && iconPositionLabel === 'start' && <BadgeIcon icon={options.badge.labelIcon.icon} iconPosition={iconPositionLabel} />}
+        {options?.badge?.label && <div className="ec-badge-title">{options.badge.label}</div>}
+        {options?.badge?.labelIcon?.icon && iconPositionLabel === 'end' && <BadgeIcon icon={options.badge.labelIcon.icon} iconPosition={iconPositionLabel} />}
+      </div>
+      <div className={classNames('ec-badge-split-value', `${badgeClass}-value`)}>
+        {options?.badge?.valueIcon?.icon && iconPositionValue === 'start' && <BadgeIcon icon={options.badge.valueIcon.icon} iconPosition={iconPositionValue} />}
+        {options?.badge?.value && <div className="ec-badge-title">{options.badge.value}</div>}
+        {options?.badge?.valueIcon?.icon && iconPositionValue === 'end' && <BadgeIcon icon={options.badge.valueIcon.icon} iconPosition={iconPositionValue} />}
+      </div>
+    </div>
+  );
+}
+
+export { BadgeSolid, BadgeLight, BadgeLink, BadgeOutline, BadgePreset, BadgeSplit };
--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,4 @@
 export { default as DayPicker } from './components/DayPicker/DayPicker';
 export { default as HourPicker } from './components/HourPicker/HourPicker';
 export { default as Loading } from './components/Loading/Loading';
+export { BadgeSolid, BadgeLight, BadgeLink, BadgeOutline, BadgePreset, BadgeSplit } from './components/Badge/Badge';
